b1f085baed73bf3bd4a82d85d3c38cbf8e1739b8
Karmjeet Nehal <karmnamol@gmail.com>
2022-03-05 10:05:58 -0800

    updates

---
+++ react-portfolio/src/components/Header.js
@@ -0,0 +1,33 @@
+import React from "react";
+//import { MenuOutlined } from "@material-ui/icons";
+import { Link } from "react-router-dom";
+import "./styles/Header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <nav>
+        <div className="logo">
+          <h1> KarmJeet Nehal</h1>
+        </div>
+        <ul className="ul-items">
+          <li>
+            <Link to="/">About Me</Link>
+          </li>
+          <li>
+            <Link to="/portfolio">Portfolio</Link>
+          </li>
+          <li>
+            <Link to="/">Resume</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          {/* <div className="hamburger-menu">
+            <MenuOutlined className="menu" />
+          </div> */}
+        </ul>
+      </nav>
+    </div>
+  );
+};
