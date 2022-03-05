f0f3148639c5c077dac2638d1d0c649d69dc2bdb
Karmjeet Nehal <karmnamol@gmail.com>
2022-03-05 13:50:37 -0800

    changes

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,31 +1,24 @@
-
-import './App.css';
-import Header from './components/Header'
-import Second from './components/Second'
-import Section from './components/Section'
-import Contact from './components/Contact'
-import Portfolio from './components/Portfolio'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
-
-
-
+import "./App.css";
+import Header from "./components/Header";
+import Second from "./components/Second";
+import Section from "./components/Section";
+import Contact from "./components/Contact";
+import Portfolio from "./components/Portfolio";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Header/>
-      {/* <Section /> */}
-      <Routes>
-        <Route path='/' element={<Section />}/>
-        <Route path='/contact' element={<Contact />}/>
-        <Route path='/portfolio' element={<Portfolio />}/>
-        <Route path='/second' element={<Second />}/>
-
-      </Routes>
-
-     
-    </div>
+      <div className="App">
+        <Header />
+        {/* <Section /> */}
+        <Routes>
+          <Route path="/" element={<Section />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/second" element={<Second />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
--- react-portfolio/src/components/Header.js
+++ react-portfolio/src/components/Header.js
@@ -23,11 +23,37 @@ export default function Header() {
           <li>
             <Link to="/contact">Contact</Link>
           </li>
-          {/* <div className="hamburger-menu">
-            <MenuOutlined className="menu" />
-          </div> */}
         </ul>
       </nav>
+
+      <footer>
+        <div class="footer">
+          <a href="https://github.com/karmjeetnamol">
+            <img
+              src="https://img.icons8.com/color/48/000000/github.png"
+              alt="Github"
+              class="icon"
+            />
+          </a>
+
+          <a href="https://www.linkedin.com/in/karmjeet-nehal-87319a228/">
+            <img
+              src="https://img.icons8.com/color/48/000000/linkedin.png"
+              alt="LinkedIn"
+              class="icon"
+            />
+          </a>
+
+          <p>
+            &copy; Karmjeet Nehal || made with{" React"}
+            <img
+              id="react-icon"
+              src="https://img.icons8.com/color/48/000000/react-native.png"
+              alt="React"
+            />
+          </p>
+        </div>
+      </footer>
     </div>
   );
-};
+}
